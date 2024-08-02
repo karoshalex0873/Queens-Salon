ea6f2d43e66f5c59df2e3e1ce018a3ab90df7a66
Alex Karobia <akarobia30@gmail.com>
2024-08-02 14:59:34 +0300

    Creted a componet for Hair coloring

--- src/containers/Haircolor.jsx
+++ src/containers/Haircolor.jsx
@@ -11,6 +11,11 @@ import emmaRoberts from '../assets/Hero.png'; // Replace with actual image
 import personImage1 from '../assets/Hero.png'; // Replace with actual images
 import personImage2 from '../assets/Hero.png'; // Replace with actual images
 import personImage3 from '../assets/Hero.png'; // Replace with actual images
+import personImage4 from '../assets/Hero.png'; // Replace with actual images
+import personImage5 from '../assets/Hero.png'; // Replace with actual images
+import personImage6 from '../assets/Hero.png'; // Replace with actual images
+import personImage7 from '../assets/Hero.png'; // Replace with actual images
+import personImage8 from '../assets/Hero.png'; // Replace with actual images
 
 const Haircolor = () => {
   useEffect(() => {
@@ -39,35 +44,35 @@ const Haircolor = () => {
       </div>
       
       <div className="relative mb-8" data-aos="fade-up" data-aos-delay="900">
-            <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-primaryDark mb-4 text-center">Number of Appointments</h2>
-            <p className="text-base md:text-lg text-primaryDark text-start mb-4">We have received over 1,200 appointments for hair coloring services this year.</p>
-            <div className="relative flex justify-center py-6">
-                <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '0', zIndex: 3 }}>
-                <img src={personImage1} alt="Person 1" className="w-full h-full object-cover rounded-full" />
-                </div>
-                <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '20px', zIndex: 2 }}>
-                <img src={personImage2} alt="Person 2" className="w-full h-full object-cover rounded-full" />
-                </div>
-                <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '40px', zIndex: 1 }}>
-                <img src={personImage3} alt="Person 3" className="w-full h-full object-cover rounded-full" />
-                </div>
-                <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '40px', zIndex: 1 }}>
-                <img src={personImage3} alt="Person 3" className="w-full h-full object-cover rounded-full" />
-                </div>
-                <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '40px', zIndex: 1 }}>
-                <img src={personImage3} alt="Person 3" className="w-full h-full object-cover rounded-full" />
-                </div>
-                <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '40px', zIndex: 1 }}>
-                <img src={personImage3} alt="Person 3" className="w-full h-full object-cover rounded-full" />
-                </div>
-                <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '40px', zIndex: 1 }}>
-                <img src={personImage3} alt="Person 3" className="w-full h-full object-cover rounded-full" />
-                </div>
-                <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '40px', zIndex: 1 }}>
-                <img src={personImage3} alt="Person 3" className="w-full h-full object-cover rounded-full" />
-                </div>
+        <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-primaryDark mb-4 text-center">Number of Appointments</h2>
+        <p className="text-base md:text-lg text-primaryDark text-center mb-4">We have received over 1,200 appointments for hair coloring services this year.</p>
+        <div className="relative flex justify-center py-6">
+            <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '0', zIndex: 8 }}>
+            <img src={personImage1} alt="Person 1" className="w-full h-full object-cover rounded-full" />
             </div>
-          </div>
+            <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '20px', zIndex: 7 }}>
+            <img src={personImage2} alt="Person 2" className="w-full h-full object-cover rounded-full" />
+            </div>
+            <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '40px', zIndex: 6 }}>
+            <img src={personImage3} alt="Person 3" className="w-full h-full object-cover rounded-full" />
+            </div>
+            <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '60px', zIndex: 5 }}>
+            <img src={personImage4} alt="Person 4" className="w-full h-full object-cover rounded-full" />
+            </div>
+            <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '80px', zIndex: 4 }}>
+            <img src={personImage5} alt="Person 5" className="w-full h-full object-cover rounded-full" />
+            </div>
+            <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '100px', zIndex: 3 }}>
+            <img src={personImage6} alt="Person 6" className="w-full h-full object-cover rounded-full" />
+            </div>
+            <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '120px', zIndex: 2 }}>
+            <img src={personImage7} alt="Person 7" className="w-full h-full object-cover rounded-full" />
+            </div>
+            <div className="absolute w-12 h-12 rounded-full border-4 border-white bg-gray-100 flex items-center justify-center overflow-hidden" style={{ left: '140px', zIndex: 1 }}>
+            <img src={personImage8} alt="Person 8" className="w-full h-full object-cover rounded-full" />
+            </div>
+        </div>
+        </div>
 
       
       <div className="mb-8" data-aos="fade-up" data-aos-delay="1200">
